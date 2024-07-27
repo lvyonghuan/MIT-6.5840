ef3057fd936f03f38032243189114a17cf72847e
lvyonghuan <1914128278@qq.com>
2024-07-27 16:59:30 +0800

    lab2

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,24 +1,30 @@
 package kvsrv
 
-import "6.5840/labrpc"
+import (
+	"6.5840/labrpc"
+)
 import "crypto/rand"
 import "math/big"
 
 type Clerk struct {
 	server *labrpc.ClientEnd
-	// You will have to modify this struct.
+
+	uid       int32
+	clientUID int64
 }
 
-func nrand() int64 {
-	max := big.NewInt(int64(1) << 62)
-	bigx, _ := rand.Int(rand.Reader, max)
-	x := bigx.Int64()
+func nRand() int64 {
+	maxNum := big.NewInt(int64(1) << 62)
+	bigX, _ := rand.Int(rand.Reader, maxNum)
+	x := bigX.Int64() + 1 //不生成0
 	return x
 }
 
 func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.server = server
+	ck.clientUID = nRand()
+	ck.uid = 0
 	// You'll have to add code here.
 	return ck
 }
@@ -34,9 +40,18 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
+	ck.uid++
 
-	// You will have to modify this function.
-	return ""
+	var (
+		isSuccess = false
+		reply     = &GetReply{}
+	)
+
+	for !isSuccess {
+		isSuccess = ck.server.Call("KVServer.Get", &GetArgs{Key: key, UID: ck.uid, ClientUID: ck.clientUID}, reply)
+	}
+
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -48,8 +63,18 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
-	// You will have to modify this function.
-	return ""
+	ck.uid++
+
+	var (
+		isSuccess = false
+		reply     = &PutAppendReply{}
+	)
+
+	for !isSuccess {
+		isSuccess = ck.server.Call("KVServer."+op, &PutAppendArgs{Key: key, Value: value, UID: ck.uid, ClientUID: ck.clientUID}, reply)
+	}
+
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,28 +14,118 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-type KVServer struct {
-	mu sync.Mutex
+type requestInfo struct {
+	uid     int32
+	content string
+}
 
-	// Your definitions here.
+type KVServer struct {
+	mu sync.RWMutex
+
+	rmu sync.RWMutex
+
+	stores  map[string]string
+	seenReq map[int64]*requestInfo
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.RLock()
+	defer kv.mu.RUnlock()
+
+	kv.rmu.RLock()
+	if value, ok := kv.seenReq[args.ClientUID]; ok {
+		kv.rmu.RUnlock()
+		if value.uid == args.UID {
+			reply.Value = value.content
+			return
+		}
+	} else {
+		kv.rmu.RUnlock()
+	}
+
+	key := args.Key
+	if value, isExist := kv.stores[key]; !isExist {
+		reply.Value = ""
+
+		//kv.rmu.Lock()
+		//kv.seenReq[args.ClientUID] = &requestInfo{
+		//	uid:     args.UID,
+		//	content: "",
+		//}
+		//kv.rmu.Unlock()
+		return
+	} else {
+		reply.Value = value
+	}
+
+	//kv.rmu.Lock()
+	//kv.seenReq[args.ClientUID] = &requestInfo{
+	//	uid:     args.UID,
+	//	content: reply.Value,
+	//}
+	//kv.rmu.Unlock()
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.rmu.RLock()
+	if value, ok := kv.seenReq[args.ClientUID]; ok {
+		kv.rmu.RUnlock()
+		if value.uid == args.UID {
+			reply.Value = value.content
+			return
+		}
+	} else {
+		kv.rmu.RUnlock()
+	}
+
+	kv.stores[args.Key] = args.Value
+
+	//kv.rmu.Lock()
+	//kv.seenReq[args.ClientUID] = &requestInfo{
+	//	uid:     args.UID,
+	//	content: value,
+	//}
+	//kv.rmu.Unlock()
+
+	//reply.Value = value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.rmu.RLock()
+	if value, ok := kv.seenReq[args.ClientUID]; ok {
+		kv.rmu.RUnlock()
+		if value.uid == args.UID {
+			reply.Value = value.content
+			return
+		}
+	} else {
+		kv.rmu.RUnlock()
+	}
+
+	key := args.Key
+	value := args.Value
+	reply.Value = kv.stores[key]
+	kv.stores[key] += value
+
+	kv.rmu.Lock()
+	kv.seenReq[args.ClientUID] = &requestInfo{
+		uid:     args.UID,
+		content: reply.Value,
+	}
+	kv.rmu.Unlock()
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
-	// You may need initialization code here.
+	kv.stores = make(map[string]string)
+	kv.seenReq = make(map[int64]*requestInfo)
 
 	return kv
 }
